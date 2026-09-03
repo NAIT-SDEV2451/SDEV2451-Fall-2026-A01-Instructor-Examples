977c42ba262edc90e61eaa0813b5d2c64e53898c
Daniel Mouris <dgmouris@gmail.com>
2026-09-03 09:39:44 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/fleet/models.py
+++ 1_backend_review_start/vehiclefleet_backend/fleet/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+
+# vehicle/car
+# remember this is a database table.
+# that is a python class definition.
+# that's what an orm does.
+class Vehicle(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    licence_plate = models.CharField(max_length=100)
+
+    # in the admin when when you print out the model
+    # you want something intelligible
+    def __str__(self):
+        return f" {self.make}, {self.model}, {self.year}, ({self.licence_plate})"
+
+
+# driver
+
+# manufacturer?
+
+# location?
+
+# trip

1117d4f910e0f1e4019f44ad2e076c8e14c8fd79
Daniel Mouris <dgmouris@gmail.com>
2026-09-03 09:28:46 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/vehiclefleet_backend/urls.py
+++ 1_backend_review_start/vehiclefleet_backend/vehiclefleet_backend/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
 
+# include allows use to import from a custom app or
+# a third party app.
+from django.urls import path, include
+
+# I want to include the fleet urls.
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/v1/",
+        include("fleet.urls"),
+    ),
 ]

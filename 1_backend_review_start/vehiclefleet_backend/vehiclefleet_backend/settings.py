76f821e30134d845a16c75aeaae7e9d0e2b325a6
Daniel Mouris <dgmouris@gmail.com>
2026-09-03 09:25:24 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/vehiclefleet_backend/settings.py
+++ 1_backend_review_start/vehiclefleet_backend/vehiclefleet_backend/settings.py
@@ -73,3 +73,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
+# DJango rest framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": {
+        "rest_framework.permissions.AllowAny",
+    }
+}

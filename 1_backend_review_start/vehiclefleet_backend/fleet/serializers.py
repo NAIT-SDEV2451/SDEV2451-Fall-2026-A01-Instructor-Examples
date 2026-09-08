490e4946605887c9acfbd41ec93f5539672c235d
Daniel Mouris <dgmouris@gmail.com>
2026-09-08 08:52:12 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/fleet/serializers.py
+++ 1_backend_review_start/vehiclefleet_backend/fleet/serializers.py
@@ -5,6 +5,18 @@ from rest_framwork import serializers  # brings in the library
 # import all of my models
 from fleet.models import Driver, Trip, Vehicle
 
+
 # the model serializer converts the database column/fields into a
 # json serializable format, and also adds the deserialization validation
 # so that you don't have junk in your database.
+# you can always take a look here: https://www.django-rest-framework.org/api-guide/serializers/#drf-pydantic
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle  # the orm model
+        fields = "__all__"  # this means all of the columns.
+
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver  # the orm model
+        fields = "__all__"  # this means all of the columns.

fb657edbb703456ae8006f12d84c8d3f44486367
Daniel Mouris <dgmouris@gmail.com>
2026-09-08 08:39:17 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/fleet/admin.py
+++ 1_backend_review_start/vehiclefleet_backend/fleet/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Vehicle, Driver, Trips
+from .models import Vehicle, Driver, Trip
 
 admin.site.register(Vehicle)
 admin.site.register(Driver)
-admin.site.register(Trips)
+admin.site.register(Trip)

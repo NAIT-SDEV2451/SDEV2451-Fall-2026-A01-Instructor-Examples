0e479527c4ac829ed4960b5f892b5f85251a4139
Daniel Mouris <dgmouris@gmail.com>
2026-09-14 10:20:16 -0600

    class progress pushed

---
+++ 2_frontend_review_start/vehiclefleet_frontend/src/pages/VehiclesAndDriversPage.jsx
@@ -0,0 +1,17 @@
+
+
+// name your components the same as the file.
+export default function VehiclesAndDriversPage() {
+  return <div className="flex flex-col gap-8">
+    <section>
+      <h2 className="text-xl font-semibold mb-3">
+        Vehicles
+      </h2>
+    </section>
+    <section>
+      <h2 className="text-xl font-semibold mb-3">
+        Vehicles
+      </h2>
+    </section>
+  </div>
+}

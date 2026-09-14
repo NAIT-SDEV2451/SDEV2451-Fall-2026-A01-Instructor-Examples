bf3f0c119654a13f6aaeca280b8ce31eaea28c91
Daniel Mouris <dgmouris@gmail.com>
2026-09-14 11:21:04 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/src/components/TripForm.jsx
+++ 2_frontend_review_start/vehiclefleet_frontend/src/components/TripForm.jsx
@@ -38,25 +38,29 @@ export default function TripForm({
   }
 
   return <div className="card bg-base-100 shadow-md w-full max-w-xl">
-      <div className="card-body gap-5">
-        <form  className="flex flex-col gap-5">
-          <div className="form-control w-full">
-            <div className="label pb-1">
-              <span className="label-text font-medium">Vehicle</span>
-            </div>
-            <select
-              name="vehicle"
-              className="select select-bordered w-full"
-              required
-            >
-              <option value="" disabled>Select a vehicle</option>
-
-            </select>
+    <div className="card-body gap-5">
+      <form className="flex flex-col gap-5">
+        <div className="form-control w-full">
+          <div className="label pb-1">
+            <span className="label-text font-medium">Vehicle</span>
           </div>
-          <div className="card-actions justify-end pt-2">
-            <button type="submit" className="btn btn-primary">Create Trip</button>
-          </div>
-        </form>
-      </div>
+          <select
+            name="vehicle"
+            className="select select-bordered w-full"
+            required
+          >
+            <option value="" disabled>Select a vehicle</option>
+            {vehicles.map((vehicle) => {
+              return <option key={vehicle.id}>
+                {vehicle.year} {vehicle.make} {vehicle.model} - {vehicle.license_plate}
+              </option>
+            })}
+          </select>
+        </div>
+        <div className="card-actions justify-end pt-2">
+          <button type="submit" className="btn btn-primary">Create Trip</button>
+        </div>
+      </form>
     </div>
+  </div>
 }

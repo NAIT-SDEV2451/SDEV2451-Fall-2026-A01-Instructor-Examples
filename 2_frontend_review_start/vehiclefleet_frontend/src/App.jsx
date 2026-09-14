9c4e15ea6bd12f1e297eea09e80b80ac742d3525
Daniel Mouris <dgmouris@gmail.com>
2026-09-14 10:29:28 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/src/App.jsx
+++ 2_frontend_review_start/vehiclefleet_frontend/src/App.jsx
@@ -1,8 +1,36 @@
+// we're going to import everything we need
+// from react router. docs: https://reactrouter.com/home
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink
+} from 'react-router-dom'
+
 function App() {
+  // wrap the entire thing in a browser router.
   return (
-    <div>
-      <h1>Fleet Manager</h1>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-base-200">
+        {/* This a navbar. */}
+        <nav className="navbar bg-base-100 shadow px-6">
+          <div className="navbar-start">
+            <span className="text-lg font-bold">Fleet Manager</span>
+          </div>
+          <div className="navbar-end gap-2">
+
+          </div>
+        </nav>
+
+        {/* The main content, render the routes. */}
+        <main className="p-6 max-w-6xl mx-auto">
+          <Routes>
+            {/* each route is going to have a path
+            and an element which is a component */}
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 

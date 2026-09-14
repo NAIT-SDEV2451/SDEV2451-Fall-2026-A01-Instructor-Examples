dbc6c8cdd1b0609aab40c83bbc5e93c7df248904
Daniel Mouris <dgmouris@gmail.com>
2026-09-14 11:12:41 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
+++ 2_frontend_review_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
@@ -1,7 +1,22 @@
+import TripForm from "../components/TripForm"
+// let's import the mock data here.
+import { VEHICLES, DRIVERS } from "../mockData"
+
+
 export default function CreateTripPage() {
+  // let's make a handler for the submission.
+  const handleSubmit = (formData) => {
+    console.log(formData)
+  }
+
   return <div>
     <h2 className="text-xl font-semibold mb-4">
       Create a new Trip
     </h2>
+    <TripForm
+      onSubmit={handleSubmit}
+      vehicles={VEHICLES}
+      drivers={DRIVERS}
+    />
   </div>
 }

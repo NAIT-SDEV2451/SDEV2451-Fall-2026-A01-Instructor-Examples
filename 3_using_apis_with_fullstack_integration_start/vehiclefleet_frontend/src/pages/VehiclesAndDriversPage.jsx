2e49e7a9d42874b8fb5fc99b458f65d04413da0f
Daniel Mouris <dgmouris@gmail.com>
2026-09-15 10:26:21 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/VehiclesAndDriversPage.jsx
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/VehiclesAndDriversPage.jsx
@@ -1,13 +1,29 @@
 import VehicleList from '../components/VehicleList'
 import DriverList from '../components/DriverList'
 import { VEHICLES, DRIVERS } from '../mockData'
+// let's import our hook here which takes care of the request
+// end to end with our state.
+import { useVehicles } from '../hooks/useVehicles'
 
 function VehiclesAndDriversPage() {
+  // load the vehicles
+  const {
+    vehicles, // the data,
+    isLoading: loadingVehicles, //obj destructuring techinque
+  } = useVehicles()
+
+  console.log("vehicles", vehicles)
+  console.log("loadingVehicles", loadingVehicles)
+
   return (
     <div className="flex flex-col gap-8">
       <section>
         <h2 className="text-xl font-semibold mb-3">Vehicles</h2>
-        <VehicleList vehicles={VEHICLES} />
+        {/* Let's use a ternary to use the new fetching  */}
+        { loadingVehicles
+          ? <span className="loading loading-spinner loading-md"></span>
+          : <VehicleList vehicles={vehicles} />
+        }
       </section>
 
       <section>

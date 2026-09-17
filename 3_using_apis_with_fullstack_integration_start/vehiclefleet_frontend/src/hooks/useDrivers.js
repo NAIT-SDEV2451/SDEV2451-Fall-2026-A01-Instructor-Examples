1303ac642803443d6e2278223e01d6cf5627ce20
Daniel Mouris <dgmouris@gmail.com>
2026-09-17 08:33:59 -0600

    class progress pushed

---
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/hooks/useDrivers.js
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { fetchDrivers } from '../api/fleet'
+
+export async function useDrivers() {
+  const {
+    data: drivers = [],
+    isLoading,
+    isError,
+    error
+  } = useQuery({
+    queryKey: ['drivers'],
+    queryFn: fetchDrivers
+  })
+  return {
+    drivers,
+    isLoading,
+    isError,
+    error,
+  }
+}

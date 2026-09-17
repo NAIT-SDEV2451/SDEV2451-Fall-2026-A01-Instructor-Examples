71ee65a6c9a8524a87628c555c8152c783a7b695
Daniel Mouris <dgmouris@gmail.com>
2026-09-17 08:48:06 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/hooks/useTrips.js
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/hooks/useTrips.js
@@ -1,6 +1,28 @@
+import {
+  useQuery
+} from '@tanstack/react-query'
 
+import { fetchTrips } from '../api/fleet'
 
 // there's multiple ways to do this
 // because we're going to make two hooks
 // one for create one for fetching but
 // you could do one if you wanted to.
+
+export function useTrips() {
+  const {
+    data: trips = [],
+    isLoading,
+    isError,
+    error
+  } = useQuery({
+    queryKey: ['trips'],
+    queryFn: fetchTrips
+  });
+  return {
+    trips,
+    isLoading,
+    isError,
+    error,
+  }
+}

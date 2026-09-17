6d96340ab635c9dc1d4dc7a07ef4569636292541
Daniel Mouris <dgmouris@gmail.com>
2026-09-17 09:16:02 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
@@ -1,20 +1,32 @@
 import { useNavigate } from 'react-router-dom'
 import TripForm from '../components/TripForm'
-import { VEHICLES, DRIVERS } from '../mockData'
+import { useDrivers } from '../hooks/useDrivers'
+import { useVehicles } from '../hooks/useVehicles'
+
 
 function CreateTripPage() {
   const navigate = useNavigate()
 
+  const { drivers } = useDrivers()
+  const { vehicles } = useVehicles()
+
+
   function handleSubmit(formData) {
     // In a real app: POST to /api/v1/trips/ then navigate
     console.log('New trip submitted:', formData)
+
+
     navigate('/trips')
   }
 
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">Create a New Trip</h2>
-      <TripForm vehicles={VEHICLES} drivers={DRIVERS} onSubmit={handleSubmit} />
+     <TripForm
+      vehicles={vehicles}
+      drivers={drivers}
+      onSubmit={handleSubmit}
+    />
     </div>
   )
 }

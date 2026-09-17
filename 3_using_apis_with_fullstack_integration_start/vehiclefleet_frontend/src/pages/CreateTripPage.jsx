9efdebf0e0b0f816b251b2642537f794f20b6ec5
Daniel Mouris <dgmouris@gmail.com>
2026-09-17 09:20:47 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
@@ -3,6 +3,8 @@ import TripForm from '../components/TripForm'
 import { useDrivers } from '../hooks/useDrivers'
 import { useVehicles } from '../hooks/useVehicles'
 
+// import my useCreateTrip mutation hook.
+import { useCreateTrip } from '../hooks/useTrips'
 
 function CreateTripPage() {
   const navigate = useNavigate()
@@ -10,23 +12,37 @@ function CreateTripPage() {
   const { drivers } = useDrivers()
   const { vehicles } = useVehicles()
 
+  const {
+    mutate: createTripMutation, // will execute the mutation function
+    // you need to call this with the data.
+    isPending
+  } = useCreateTrip()
 
   function handleSubmit(formData) {
     // In a real app: POST to /api/v1/trips/ then navigate
     console.log('New trip submitted:', formData)
+    createTripMutation(formData, {
+      onSuccess: () => {
+        navigate('/trips')
+      }
+    })
+  }
 
-
-    navigate('/trips')
+  if (isPending) {
+    return <div>
+      <h2 className="text-xl font-semibold mb-4">Create a New Trip</h2>
+      <span className="loading loading-spinner loading-md"></span>
+    </div>
   }
 
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">Create a New Trip</h2>
-     <TripForm
-      vehicles={vehicles}
-      drivers={drivers}
-      onSubmit={handleSubmit}
-    />
+      <TripForm
+        vehicles={vehicles}
+        drivers={drivers}
+        onSubmit={handleSubmit}
+      />
     </div>
   )
 }

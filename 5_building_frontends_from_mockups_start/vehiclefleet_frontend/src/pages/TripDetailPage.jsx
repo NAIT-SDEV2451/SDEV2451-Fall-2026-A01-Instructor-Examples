19d349de8c0ddf4938acecd1102c5d145f447fb4
Daniel Mouris <dgmouris@gmail.com>
2026-09-22 09:35:24 -0600

    class progress pushed

--- 5_building_frontends_from_mockups_start/vehiclefleet_frontend/src/pages/TripDetailPage.jsx
+++ 5_building_frontends_from_mockups_start/vehiclefleet_frontend/src/pages/TripDetailPage.jsx
@@ -1,14 +1,27 @@
 // let's get the first trip from the mockdata
 import { TRIPS } from "../mockData";
-
+import BackButton from "../components/BackButton";
 
 export default function TripDetailPage() {
-  // let's select the first one
-  const trip = TRIPS[0]
-  console.log(trip)
+  // let's select the one
+  const trip = TRIPS[4] // in our mock data the endtime for the 4 is null
+  // create a variable called in progress to see if the end time is null
+  const isInProgress = trip.end_time === null
+
   return <>
-    {/* <div className="flex flex-col gap-6"> */}
-      <h1>trip</h1>
-    {/* </div> */}
+    <div className="flex flex-col gap-6">
+      <div>
+        {/* back button */}
+        <BackButton to={`/trips`} label="Back to Trips" />
+        {/* Title */}
+        <div class="flex items-center gap-3">
+          <h1 className="text-3xl font-bold">Trip #{trip.id}</h1>
+          { isInProgress &&
+            <span className="badge badge-info">In Progress</span>
+          }
+        </div>
+      </div>
+
+    </div>
   </>
 }

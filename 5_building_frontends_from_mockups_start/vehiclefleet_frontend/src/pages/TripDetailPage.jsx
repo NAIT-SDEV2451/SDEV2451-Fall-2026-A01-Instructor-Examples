f5de0f79c7f8c864da37268ac0439fd4ed6122d3
Daniel Mouris <dgmouris@gmail.com>
2026-09-22 09:29:01 -0600

    class progress pushed

--- 5_building_frontends_from_mockups_start/vehiclefleet_frontend/src/pages/TripDetailPage.jsx
+++ 5_building_frontends_from_mockups_start/vehiclefleet_frontend/src/pages/TripDetailPage.jsx
@@ -5,10 +5,10 @@ import { TRIPS } from "../mockData";
 export default function TripDetailPage() {
   // let's select the first one
   const trip = TRIPS[0]
-
+  console.log(trip)
   return <>
-    <div className="flex flex-col gap-6">
-      <h1>trip: {trip}</h1>
-    </div>
+    {/* <div className="flex flex-col gap-6"> */}
+      <h1>trip</h1>
+    {/* </div> */}
   </>
 }
